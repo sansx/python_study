662beff474188eda47dd3227f26ac0c5061f0087
sansx <xiathxth@gmail.com>
2018-09-03 17:43:54 +0800

    lesson4

--- PythonCrashCourse/data visualization/lesson4/highs_lows.py
+++ PythonCrashCourse/data visualization/lesson4/highs_lows.py
@@ -1,5 +1,6 @@
 import csv
 from matplotlib import pyplot as plt
+from datetime import datetime
 # import os
 
 # cwd = os.getcwd()  # Get the current working directory (cwd)
@@ -12,18 +13,24 @@ with open(filename) as f:
 
     header_row = next(render)
 
-    highs = []
+    dates, highs = [],[]
     for row in render:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+
         high = int(row[1])
         highs.append(high)
 
 
 fig = plt.figure(dpi=128, figsize=(10,6))
 
-plt.plot(highs, c='red')
+plt.plot(dates, highs, c='red')
 
 plt.title("Daily high temperatures, July 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
+
+fig.autofmt_xdate()
+
 plt.ylabel("Temperature (F)", fontsize=16)
 
 plt.tick_params(axis='both', which='major', labelsize=16)
