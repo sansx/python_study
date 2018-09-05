12f6c5561a80e44e9d93e3d894d02b67ed0a5bcb
sansx <xiathxth@gmail.com>
2018-09-05 17:17:02 +0800

    can edit items

--- PythonCrashCourse/learning_log/learning_logs/urls.py
+++ PythonCrashCourse/learning_log/learning_logs/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     # 主页
     re_path(r'^$', views.index, name='index'),
     re_path(r'^topics/$', views.topics, name='topics'),
-    re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
+    re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    re_path(r'^new_topic/$', views.new_topic, name='new_topic'),
+    re_path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry')
 ]
