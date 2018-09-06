a7dc635fbbcc8d18ff303a76a164d91236625d34
sansx <xiathxth@gmail.com>
2018-09-06 17:25:29 +0800

    user login

--- PythonCrashCourse/learning_log/learning_logs/views.py
+++ PythonCrashCourse/learning_log/learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -12,13 +12,13 @@ def index(request):
 
     return render(request, 'learning_logs/index.html')
 
-
+@login_required
 def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics':topics}
     return render(request, 'learning_logs/topics.html', context)
 
-
+@login_required
 def topic(request, topic_id, entry_id=0):
     if 'delete' in request.path_info and Entry.objects.get(id=entry_id):
         entry = Entry.objects.get(id=entry_id)
@@ -30,7 +30,7 @@ def topic(request, topic_id, entry_id=0):
     context = {'topic':topic, 'entries':entries}
     return render(request, 'learning_logs/topic.html', context)
 
-
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
@@ -44,7 +44,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
-
+@login_required
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     if request.method != "POST":
@@ -61,7 +61,7 @@ def new_entry(request, topic_id):
     context = {'topic':topic, 'form':form}
     return render(request, 'learning_logs/new_entry.html', context)
 
-
+@login_required
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
