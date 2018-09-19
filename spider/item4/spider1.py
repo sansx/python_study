2221168a2ed7605bbe6c12a8432c1b891ad2048a
sansx <xiathxth@gmail.com>
2018-09-19 23:12:46 +0800

    update

--- spider/item4/spider1.py
+++ spider/item4/spider1.py
@@ -3,3 +3,7 @@ from bs4 import BeautifulSoup
 # cd scrapingEnv/  移动到环境目录
 # source bin/activate  开始运行环境
 # deactivate  退出环境
+import requests
+r = requests.get("http://www.pythonscraping.com/pages/page1.html")
+bsObj = BeautifulSoup(r.text,features='html.parser')
+print(bsObj.h1)
